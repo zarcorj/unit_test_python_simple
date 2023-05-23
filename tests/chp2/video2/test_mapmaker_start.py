35435caca0347643d43c08d67482c9483a5a737e
Javier <zarcorx@gmail.com>
2023-05-22 23:18:51 -0600

    Improving unit test

--- tests/chp2/video2/test_mapmaker_start.py
+++ tests/chp2/video2/test_mapmaker_start.py
@@ -1,4 +1,8 @@
+from scripts.chp2.video2.mapmaker_start import Point
 
 
 def test_make_one_point():
-    pass
+    """Test that a point can be created."""
+
+    p1 = Point("Darkota", 14.234, 16.234)
+    assert p1.get_lat_long() == (14.234, 16.234)
