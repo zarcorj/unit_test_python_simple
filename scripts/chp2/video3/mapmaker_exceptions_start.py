55b1706f11b889a3bd4623cd1dfe4f0622b8573a
Javier <zarcorx@gmail.com>
2023-05-26 14:23:07 -0600

    chp2-video3 test raises exceptions

--- scripts/chp2/video3/mapmaker_exceptions_start.py
+++ scripts/chp2/video3/mapmaker_exceptions_start.py
@@ -1,10 +1,19 @@
+"""A module for latitude and longitude points."""
 
-class Point():
+
+class Point:
+    """Class to represent a point on the earth's surface."""
+
     def __init__(self, name, latitude, longitude):
+        """Initialize the position of the point."""
         self.name = name
+
+        if not (-90 <= latitude <= 90) or not (-180 <= longitude <= 180):
+            raise ValueError("Invalid latitude, longitude combination.")
+
         self.latitude = latitude
         self.longitude = longitude
 
-
     def get_lat_long(self):
+        """Return the latitude and longitude of the point."""
         return (self.latitude, self.longitude)
