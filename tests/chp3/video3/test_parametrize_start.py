ab24c055c67f1b032b4a29f9162a60239b6beb4d
Javier <zarcorx@gmail.com>
2023-05-28 21:56:59 -0600

    chp3-video3 completed: Parametrize start

--- tests/chp3/video3/test_parametrize_start.py
+++ tests/chp3/video3/test_parametrize_start.py
@@ -1,12 +1,12 @@
 import os
 import pytest
 
-from scripts import data_processor, data_aggregator
+from scripts import data_processor, json_processor, data_aggregator
 
 
 @pytest.fixture(scope="module")
 def city_list_location():
-    return 'tests/resources/cities/'
+    return "tests/resources/cities/"
 
 
 @pytest.fixture(scope="module")
@@ -16,24 +16,22 @@ def process_data(city_list_location):
     def _specify_type(file_name_or_type):
         for f in files:
             if file_name_or_type in f:
-                if file_name_or_type != '.json':
+                if file_name_or_type != ".json":
                     data = data_processor.csv_reader(city_list_location + f)
                 else:
-                    data = data_processor.json_reader(city_list_location + f)
+                    data = json_processor.json_reader(city_list_location + f)
         return data
 
     yield _specify_type
 
 
-def test_average_atitude_per_country(process_data):
+@pytest.mark.parametrize(
+    "country,stat,expected",
+    [("Andorra", "Mean", 1641.42), ("Andorra", "Median", 1538.02), ("Argentina", "Median", 125.0)],
+)
+def test_atitude_stat_per_country(process_data, country, stat, expected):
+    """Test attitude statistic per country from data aggregator module"""
     data = process_data(file_name_or_type="clean_map.csv")
-    andorran_avg_res = data_aggregator.atitude_stat_per_country(data, 'Andorra', 'Mean')
+    andorran_avg_res = data_aggregator.atitude_stat_per_country(data, country, stat)
 
-    assert andorran_avg_res == {'Country': 'Andorra', 'Mean': 1641.42}
-
-
-def test_median_atitude_per_country(process_data):
-    data = process_data(file_name_or_type="clean_map.csv")
-    andorran_median_res = data_aggregator.atitude_stat_per_country(data, 'Andorra', 'Median')
-
-    assert andorran_median_res == {'Country': 'Andorra', 'Median': 1538.02}
+    assert andorran_avg_res == {"Country": country, stat: expected}
